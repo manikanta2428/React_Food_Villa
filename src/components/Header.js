719986a9ffac2dd25cfc35a62a960cadc9cfba98
Matta Manikanta <matta@karomi.com>
2024-02-22 14:20:28 +0530

    Added another Li to check

--- src/components/Header.js
+++ src/components/Header.js
@@ -20,6 +20,7 @@ const Header = () => {
                     <ul>
                             <li>Home</li>
                             <li>About</li>
+                            <li>Contact</li>
                             <li>Cart</li>
                     </ul>
                     </div>
