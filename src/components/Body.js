f27f98e1a5bf5d3e4af3bc9cad8ee13e29176b61
Matta Manikanta <matta@karomi.com>
2024-02-22 23:50:25 +0530

    Search Functionality By using useState

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,14 +1,42 @@
 import RestaurentCard from "./RestaurentCard";
 import { restrautList } from "../Constants";
+import { useState } from "react";
 
 const Body = () =>{
+        const getFilterData = (searchText,restaurents) => {
+                const filteredData = restaurents.filter(x => x.data.name.includes(searchText));
+                return filteredData;
+
+        }
+        const [searchText,setSearchText] = useState("Hi");
+        const[restaurents,setRestaurents] = useState(restrautList);
     return(
+           <>
+           <div className="search-container">
+                <input type="text" 
+                className="input" 
+                placeholder="Search Here" 
+                value={searchText}
+                onChange={(e) => {
+                        setSearchText(e.target.value);
+                }}
+                />
+                <button className="serach-button" onClick={()=> {
+                        const filterData = getFilterData(searchText,restaurents);
+                        setRestaurents(filterData);
+
+                }
+                        
+                        }>Search </button>
+           </div>
             <div className="AllCards">
-                    {restrautList.map((restaurent) =>{
+                    {restaurents.map((restaurent) =>{
                             return <RestaurentCard {...restaurent.data} key={restaurent.data.id}/>
                     })}
                     
             </div>
+           </>
+
             
     );
 }
